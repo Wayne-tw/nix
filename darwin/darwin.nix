457251fbeee0c018b84a5d7bf9b8efed162181a4
Matthias Scholz <matthias.scholz@thoughtworks.com>
2024-06-24 12:52:04 +0200

    :recycle: fix deprecation

--- darwin/darwin.nix
+++ darwin/darwin.nix
@@ -33,8 +33,7 @@
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
-  fonts.fontDir.enable = true;
-  fonts.fonts = [
+  fonts.packages = [
     pkgs.atkinson-hyperlegible
     pkgs.jetbrains-mono
   ];
