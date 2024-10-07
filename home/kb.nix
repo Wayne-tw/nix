f65936263a8f8dfca43de243d1527ddb2a238df3
Matthias Scholz <matthias.scholz@thoughtworks.com>
2024-10-07 10:10:50 +0200

    ✨ obsidian - knowledge base on steroids

--- home/kb.nix
+++ home/kb.nix
@@ -7,7 +7,21 @@
 
 {
   home.packages = with pkgs; [
+    obsidian
+    # NOTE service execution is os dependent, hence a piece is in darwin.nix
     ollama
+    # Compress pngs to reduce repository footprint
+    # NOTE Needs as to be added to a precommit hook:
+    # `pre-commit install`
+    oxipng
   ];
+
+  # TODO Install some models
+  # Obsidian
+  # - nomic-embed-text
+  # - llama3.1
+  # - gemma2
+  # Emacs - Might need to be setup in emacs.nix
+  # - zephyr
 
 }
