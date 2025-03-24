f2360d7bd5e979cae7332feebae5567a00a4c6fb
Matthias Scholz <matthias.scholz@thoughtworks.com>
2025-03-24 22:42:20 +0100

    ✨ 📝 obsidian broken package workaround

--- home/kb.nix
+++ home/kb.nix
@@ -2,18 +2,35 @@
   config,
   lib,
   pkgs,
+  inputs,
   ...
 }:
 
+let
+  # NOTE obsidian 1.8.9 was broken on mac
+  #       unpacking source archive /nix/store/83ll009y30zh6wrcn2i7351gg9y45wjr-Obsidian-1.8.9.dmg
+  #       error: only HFS file systems are supported.
+  # NOTE show how packages can be pinned to a specific nixpkgs version
+  # unstable branch
+  pkgs_obsidian_1_8_7 =
+    import
+      (pkgs.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "31b5f3ba6361adde901c0c83b02f13212ccdc01f";
+        sha256 = "iR9wRkor7/lKycBZ/w1rJWanV/Wd8bcIQALOl7jJmoM=";
+      })
+      {
+        inherit (pkgs) system;
+        config.allowUnfree = true;
+      };
+in
 {
-  home.packages = with pkgs; [
-    obsidian
+  home.packages = [
+    pkgs.obsidian
+    #pkgs_obsidian_1_8_7.obsidian
     # NOTE service execution is os dependent, hence a piece is in darwin.nix
-    ollama
-    # Compress pngs to reduce repository footprint
-    # NOTE Needs as to be added to a precommit hook:
-    # `pre-commit install`
-    oxipng
+    pkgs.ollama
   ];
 
   # TODO Install some models
