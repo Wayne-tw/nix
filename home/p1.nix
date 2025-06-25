1f0ce82c3a2a611b94ad9b9e4c2e087056f16ec4
Wayneee <91533374+Wayne-tw@users.noreply.github.com>
2025-06-25 15:27:20 +0100

    Update p1.nix
    update to adapt

--- home/p1.nix
+++ home/p1.nix
@@ -7,9 +7,6 @@
 
 {
   home.packages = with pkgs; [
-    #teams
-    restish
-    # TODO move out of project related - this should be consider a core capability
     colima
     # TODO move out of project related - this should be consider a core capability
     docker
