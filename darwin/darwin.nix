dd511297af3d89728763515bcb7cdc445a2e8654
Wayneee <91533374+Wayne-tw@users.noreply.github.com>
2025-06-25 15:10:36 +0100

    Update darwin.nix
    to adapt my uses

--- darwin/darwin.nix
+++ darwin/darwin.nix
@@ -9,42 +9,10 @@
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     home-manager
-    # Local LLMs
-    ollama
-    # System Status
-    # TODO export configuration and add to this repository - including restore
-    stats
   ];
 
-  # Get ollama launched
-  # https://www.danielcorin.com/til/nix-darwin/launch-agents/
-  # TODO Move to kb.nix or consider connection if os independency should be achieved
-  # Creates a plist file at `~/Library/LaunchAgents``
-  # State: `launchctl blame gui/501/org.nixos.ollama-serve`
-  launchd = {
-    user = {
-      agents = {
-        ollama-serve = {
-          command = "${pkgs.ollama}/bin/ollama serve";
-          serviceConfig = {
-            KeepAlive = true;
-            RunAtLoad = true;
-            StandardOutPath = "/tmp/ollama.out.log";
-            StandardErrorPath = "/tmp/ollama.err.log";
-            EnvironmentVariables = {
-              OLLAMA_ORIGINS = "moz-extension://*,chrome-extension://*,safari-web-extension://*";
-            };
-          };
-        };
-      };
-    };
-  };
-
-  # Use a custom configuration.nix location.
-  # TODO keep the default location to support use of `topgrade`
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  environment.darwinConfig = "$HOME/project/config/dotfiles-slim";
-  #environment.darwinConfig = "$HOME/.config/nixpkgs/darwin";
+  # Use a custom configuration.nix location. Whereever your cloned files are.
+  environment.darwinConfig = "$HOME/nix";
 
   # Auto upgrade nix package and the daemon service.
   nix = {
@@ -61,15 +29,10 @@
       ];
       "trusted-users" = [
         # FIXME make dependent to the user name configuration - do not hard-code
-        "matthias"
+        "wayne"
       ];
     };
   };
-
-  # TODO Move to emacs or shell since it is connected to direnv
-  # https://docs.doomemacs.org/v21.12/modules/tools/direnv/
-  # NOTE disabled since it requires nix.enable
-  # services.lorri.enable = true;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs = {
@@ -81,111 +44,11 @@
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
-  # TODO Make customized screenshot shortcut declarative
-  # - default to clipboard instead to file
-  # -> https://www.reddit.com/r/NixOS/comments/17n3tcn/setting_keyboard_shortcuts_in_nix_darwin/
-
   fonts.packages = [
     pkgs.atkinson-hyperlegible
     pkgs.jetbrains-mono
   ];
 
-  services = {
-    # FIXME not working
-    #karabiner-elements = {
-    #  enable = true;
-    #};
-
-    # https://felixkratz.github.io/SketchyBar/
-    # https://mynixos.com/nix-darwin/options/services.sketchybar
-    sketchybar = {
-      enable = true;
-      # https://mynixos.com/nix-darwin/option/services.sketchybar.config
-      config = ''
-        sketchybar --bar height=24
-        sketchybar --update
-        echo "sketchybar configuration loaded.."
-      '';
-      extraPackages = [
-        pkgs.jq
-        pkgs.lua
-      ];
-    };
-    # Window Manager
-    yabai = {
-      enable = true;
-      config = {
-        layout = "float";
-        mouse_modifier = "ctrl";
-        mouse_action1 = "resize";
-        mouse_drop_action = "stack";
-        window_gap = "20";
-        window_border = "off";
-        window_opacity = "off";
-        window_shadow = "off";
-        # sketchybar awarness
-        # 24: height of sketchybar
-        external_bar = "all:24:0";
-      };
-      extraConfig = ''
-        yabai -m rule --add app='Spotify' display=east
-
-        yabai -m rule --add app='System Settings' manage=off
-
-        # Space positioning
-        yabai -m rule --add app="^Google Calendar" space=1
-        yabai -m rule --add app="^Obsidian" space=3
-        yabai -m rule --add app="^Vivaldi" space=4
-        yabai -m rule --add app="^Emacs" space=5
-        yabai -m rule --add app="^Slack" space=6
-        yabai -m rule --add app="^Microsoft Teams" space=6
-        yabai -m rule --add app="^Google Chat" space=6
-        yabai -m rule --add app="^Zoom" space=6
-        yabai -m rule --add app="^Kindle" space=7
-        yabai -m rule --add app="^Warp" space=7
-        yabai -m rule --add app="^Msty" space=7
-        yabai -m rule --add app="^Activity Monitor" space=7
-        yabai -m rule --add app="^Notion" space=8
-        yabai -m rule --add app="^Beeper" space=9
-      '';
-    };
-    # Keyboard Shortcut Manager
-    skhd = {
-      enable = true;
-      # FIXME use a different path, this is only for testing
-      # FIXME none of the commands are working
-      skhdConfig = ''
-        ctl - k : bash -c "/Users/matthias/projects/config/dotfiles-slim/config/windowswitcher/switch.sh"
-
-        # select window
-        alt + cmd - w: yabai -m query --windows \
-        | jq -r '.[] | "\(.app): \(.title)"' \
-        | /opt/homebrew/bin/choose -u -b fabd2f -c 427b58 -s 14 -n 15 -i "$@" <&0 \
-        | yabai -m query --windows \
-        | jq ".[$idx].id" \
-        | xargs yabai -m window --focus
-
-        # cycle between stacked windows
-        alt + cmd - j : yabai -m query --spaces --space \
-        | jq -re ".index" \
-        | xargs -I{} yabai -m query --windows --space {} \
-        | jq -sre 'add | map(select(."is-minimized"==false)) | sort_by(.display, .frame.y, .frame.x, .id) | . as $array | length as $array_length | index(map(select(."has-focus"==true))) as $has_index | if $has_index > 0 then nth($has_index - 1).id else nth($array_length - 1).id end' \
-        | xargs -I{} yabai -m window --focus {}
-
-        alt + cmd - k : yabai -m query --spaces --space \
-        | jq -re ".index" \
-        | xargs -I{} yabai -m query --windows --space {} \
-        | jq -sre 'add | map(select(."is-minimized"==false)) | sort_by(.display, .frame.y, .frame.x, .id) | . as $array | length as $array_length | index(map(select(."has-focus"==true))) as $has_index | if $array_length - 1 > $has_index then nth($has_index + 1).id else nth(0).id end' \
-        | xargs -I{} yabai -m window --focus {}
-      '';
-    };
-  };
-
-  # TODO Understand if there is potential to overwrite - since assignment is used
-  # FIXME have homebrew bin in shell
-  #programs.zsh.initContent = ''
-  #  export PATH="$\{PATH\}:/opt/homebrew/bin"
-  # '';
 
   # Storage optimisation
   # https://nixos.wiki/wiki/Storage_optimization
@@ -207,73 +70,23 @@
       upgrade = true;
     };
 
-    # TODO probably have to be managed via nix-homebrew
-    #taps = [
-    #  "gjbae1212/gossm"
-    #];
-
     brews = [
       # TODO find a way to install packages using go get - reduce brew usage
-      "sm"
       "gossm"
-      # Make vterm work on emacs
-      #      https://docs.doomemacs.org/v21.12/modules/term/vterm/
-      "libvterm" # doomemacs vterm requirement
-      # TODO Migrate to package management once the package is fixed
-      "choose-gui"
-      # TODO move into a template/aws fake - but curently no package
-      "awslim"
-      # Shell debugger
-      # https://bashdb.readthedocs.io/en/latest/entry-exit.html
-      "bashdb"
-      # https://zshdb.readthedocs.io/en/latest/entry-exit.html
-      "zshdb"
     ];
 
     casks = [
       # TODO Migrate to nixpkgs _1password-gui when package is not broken anymore
       "1password"
-      "beeper"
-      "disk-inventory-x"
       # TODO Migrate to nixpkgs
-      "karabiner-elements"
       "raycast"
       # TODO Migrate to nixpkgs
-      #"wezterm"
-      "vivaldi"
-      # aws specific tooling - currently unsupported package
-      # TODO Migrate to knowledge management: kb.nix
-      # TODO Migrate to engagement specific stuff
-      # TODO how to install the plugins for logseq?
-      #"logseq"
       "obsidian"
-      "notion"
-      # TODO move to project specific tools
-      #"mattermost"
-      # TODO Check if it can be replaced with the nixpkgs
       "microsoft-teams"
-      # TODO separate personal stuff
-      "orcaslicer"
-      "raspberry-pi-imager"
-      # System cleanup for manual tools
-      # NOTE try using nix capabilities as much as possible
-      # "app-cleaner"
-      # AI
-      # https://msty.app
-      # TODO Migrate to knowledge management: kb.nix
-      "msty"
-      # TODO try to get skhd working
-      "icanhazshortcut"
-      # Keyboard Configuration Management
-      "vial"
     ];
 
     masApps = {
       "1Password for Safari" = 1569813296;
-      "Bandwidth+" = 490461369;
-      "Kindle Classic" = 405399194;
-      "Kindle" = 302584613;
-      "Meeter" = 1510445899;
     };
   };
 
@@ -306,15 +119,5 @@
       NSAutomaticPeriodSubstitutionEnabled = false;
     };
   };
-
-  # Neo2 Keyboard Layout
-  # needs additional configuration of karabiner-elements as part of home-manager /home/home.nix
-  # https://github.com/Cu3PO42/gleaming-glacier/blob/5abb8c0a3fb72cafbc7ca113e5f135142d0b51c8/modules/darwin/neo2/default.nix#L9
-  # https://github.com/Cu3PO42/gleaming-glacier/blob/5abb8c0a3fb72cafbc7ca113e5f135142d0b51c8/config/karabiner/LICENSE.md?plain=1
-  system.activationScripts.extraActivation.text = ''
-    echo "INFO :: Copying Neo layout into system wide folder"
-    cp ${./neo.icns} "/Library/Keyboard Layouts/neo.icns"
-    cp ${./neo.keylayout} "/Library/Keyboard Layouts/neo.keylayout"
-  '';
 
 }
