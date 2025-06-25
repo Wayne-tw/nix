ff003acd7670c11e2765170a3accadaeb7976ac0
Wayneee <91533374+Wayne-tw@users.noreply.github.com>
2025-06-25 15:25:08 +0100

    Update editor.nix
    update to adapt

--- home/editor.nix
+++ home/editor.nix
@@ -3,7 +3,6 @@
 {
 
   home.packages = with pkgs; [
-    code-cursor
     # VS Code for AI coding agent trials
     vscode
     #vscode-with-extensions
@@ -11,47 +10,6 @@
     vscode-extensions.hashicorp.hcl
     vscode-extensions.hashicorp.terraform
     vscode-extensions.golang.go
-    # Cline MCP servers prerequisities
-    nodejs
-    # FIXME not working: nodePackages_latest."@modelcontextprotocol/server-filesystem"
-    uv
   ];
-
-  # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/by-name/ze/zed-editor/package.nix
-  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zed-editor.enable
-  programs.zed-editor = {
-    enable = true;
-
-    # FIXME using any of these configuration settings make the activate stuck.
-    # extensions = [
-    #   "basher"
-    #   "dockerfile"
-    #   "docker-compose"
-    #   "gemini"
-    #   "golangci-lint"
-    #   "gosum"
-    #   "http"
-    #   "make"
-    #   "mermaid"
-    #   "nix"
-    #   "rainbow-csv"
-    #   "rego"
-    #   "risor"
-    #   "terraform"
-    #   "markdown-oxide"
-    # ];
-
-    #   userSettings = {
-    #     # https://zed.dev/docs/configuring-zed#direnv-integration
-    #     load_direnv = "direct";
-    #     autosave = "on_focus_change";
-
-    #     agent = {
-    #       default_model = {
-    #         provider = "google.ai";
-    #         model = "gemini-2.5-pro-preview-05-06";
-    #       };
-    #     };
-    #   };
   };
 }
