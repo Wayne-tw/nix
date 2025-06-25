9fe5e3751ed6f7d3779e298db033a028f7464e49
Wayneee <91533374+Wayne-tw@users.noreply.github.com>
2025-06-25 15:17:36 +0100

    Update home.nix
    update to adapt

--- home/home.nix
+++ home/home.nix
@@ -9,17 +9,10 @@
 {
   imports = [
     ./shell.nix
-    ./wezterm.nix
-    # Consider group dev related files, e.g. dev, direnv, git, emacs, editor
-    ./dev.nix
-    ./direnv.nix
     ./git.nix
-    ./emacs.nix
     ./editor.nix
-    ./music.nix
     ./work.nix
     ./kb.nix
-    ./secretmanagement.nix
     # project specific
     ./p1.nix
     ./p2.nix
@@ -31,48 +24,17 @@
     # The home.packages option allows you to install Nix packages into your
     # environment.
     packages = with pkgs; [
-      devenv
-      ltex-ls
-      marksman
       nixd
       ripgrep
       tree
-      # Base tooling
-      drawio
-      # Support to switch windows instead of apps
-      # NOTE 'alt-tab-macos' not working to show multiple vivaldi windows - one of my use cases
-      # TODO not working using brew instead
-      # choose-gui
-      # TODO Move to macosx specific ui section
-      hidden-bar
-      # TODO move UI stuff into a separate file
-      xbar
     ];
 
-    # Home Manager is pretty good at managing dotfiles. The primary way to manage
-    # plain files is through 'home.file'.
-    #file = {
-    #  hammerspoon = lib.mkIf pkgs.stdenvNoCC.isDarwin {
-    #    source = ./../config/hammerspoon;
-    #    target = ".hammerspoon";
-    #    recursive = true;
-    #  };
-    #};
-
-    sessionVariables = {
-      # TODO Set this as part of the emacs.nix
-      EDITOR = "emacs";
-    };
-
     shellAliases = {
-      "sys-rebuild" = "sudo darwin-rebuild switch --flake ~/projects/config/dotfiles-slim --refresh";
+      "sys-rebuild" = "sudo darwin-rebuild switch --flake ~/nix --refresh";
       "sys-rollback" = "darwin-rebuild switch --rollback";
-      "sys-update" = "cd ~/projects/config/dotfiles-slim && nix flake update";
+      "sys-update" = "cd ~/nix && nix flake update";
       "sys-upgrade" = "sys-update && sys-rebuild";
       "sys-optimise" = "nix-store --optimise";
     };
-
-    # Neo2 Configuration via Karabiner Elements
-    file.".config/karabiner/karabiner.json".source = ../config/karabiner/karabiner.json;
   };
 }
