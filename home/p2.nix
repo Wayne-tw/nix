b0d578f7571ed01e8b81c95c4a5193f1e63faaf3
Wayneee <91533374+Wayne-tw@users.noreply.github.com>
2025-06-25 15:28:12 +0100

    Update p2.nix
    update to adapt

--- home/p2.nix
+++ home/p2.nix
@@ -7,12 +7,7 @@
 
 {
   home.packages = with pkgs; [
-    # TODO Find a way to manage these "globally" but outside of dotfiles-slim repository.
-    # NOTE MS Teams nixpkgs outdated and broken -> use brew
-    # teams
-    # Slack
     slack
-    # VPN client
   ];
 
 }
